c38dcb5cd47fc735901c7b8e9773770f1fc6b46b
Andrew Eames <ajerxs@gmail.com>
2020-10-29 02:49:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -113,5 +113,13 @@ def winner(board)
 end
 
 def play(board)
-  input = gets
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    winner(board) == "X" || winner(board) == "O"
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's game!"
+  end
 end
